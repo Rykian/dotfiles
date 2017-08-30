3e6be84d52f09e19931fa9e322cdaf8a4cb04c30
Thibault Lacroux <thibault.lacroux@gmail.com>
2017-08-30 17:18:13 +0200

    Reordering file loading & fixing aliases

--- .zshrc.d/50-aliases.zsh
+++ .zshrc.d/50-aliases.zsh
@@ -1,10 +1,14 @@
 alias ls=exa
-compdef ls=exa
 
-alias l=ls -1
+alias l="ls -1a"
+alias la="ll -a"
+alias lt="ll -s created"
+alias lc="ll -s created"
+alias lu="ll -s accessed"
+alias lx="ll -s Extension"
+alias lk="ll -s size -r"
 
 alias dc=docker-compose
-compdef dc=docker-compose
 
 function dcl() {
   if [ -n "$1" ]
