d039b85cc53591e57278b799fcabfe7fc3989633
Thibault Lacroux <thibault.lacroux@gmail.com>
2017-09-01 20:02:20 +0200

    Fixing terminal notifier

--- .zshrc.d/10-terminal-notifier.zsh
+++ .zshrc.d/10-terminal-notifier.zsh
@@ -1,4 +1,4 @@
 zplug "marzocchi/zsh-notify", at:WINDOWID-workaround
-zstyle ':notify:*' notifier ~/.rbenv/shims/terminal-notifier
+zstyle ':notify:*' error-title "Command failed (in #{time_elapsed} seconds)"
 zstyle ':notify:*' success-title "Command finished (in #{time_elapsed} seconds)"
 zstyle ':notify:*' command-complete-timeout 2
