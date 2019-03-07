01334066d7ae539acacc8ff31a10cc8f8767709c
Thibault Lacroux <thibault.lacroux@gmail.com>
2019-03-07 12:33:14 +0100

    Replacing rbenv ohmyzsh plugin by recommanded sh command

--- .zshrc.d/10-misc-plugins.zsh
+++ .zshrc.d/10-misc-plugins.zsh
@@ -1,6 +1,5 @@
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "Tarrasch/zsh-autoenv"
-zplug "plugins/rbenv", from:oh-my-zsh
 zplug "gnachman/iterm2-website", use:source/shell_integration/zsh, depth:1
 zplug "gnachman/iterm2-website", use:source/utilities/{imgcat,imgls}, as:command, depth:1
