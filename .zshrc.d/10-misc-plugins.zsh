75260280fcd17f8640d7c0aa6cbfccfe3f1e1785
Thibault Lacroux <thibault.lacroux@gmail.com>
2019-10-13 12:28:42 +0200

    Speed up installation by avoiding cloning the whole iterm-website

--- .zshrc.d/10-misc-plugins.zsh
+++ .zshrc.d/10-misc-plugins.zsh
@@ -1,5 +1,5 @@
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "Tarrasch/zsh-autoenv"
-zplug "gnachman/iterm2-website", use:source/shell_integration/zsh, depth:1
-zplug "gnachman/iterm2-website", use:source/utilities/{imgcat,imgls}, as:command, depth:1
+zplug "Rykian/iterm2-utilities", use:shell_integration/zsh, depth:1
+zplug "Rykian/iterm2-utilities", use:"utilities/*", as:command, depth:1
