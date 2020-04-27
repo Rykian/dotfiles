7ba76f462c80edea34557ffc7d0097b13faedff0
Thibault Lacroux <thibault.lacroux@gmail.com>
2020-04-27 11:34:02 +0200

    fix: auto-nvm behaviour

--- fish/conf.d/50-auto-nvm.fish
+++ fish/conf.d/50-auto-nvm.fish
@@ -1,21 +1,33 @@
-set NVM_USED 0
+set NVMRC_USED 0
 
 function __check_nvm --on-variable PWD --description 'Check if there is a .nvm file'
+  if [ "$NVM_DIR" != "" ]
+    __switch_nvm
+  else
+    function __enable_nvm_afer_loaded --on-variable nvm_prefix
+      if [ "$NVM_DIR" != "" ]
+        __switch_nvm
+      end
+    end
+  end
+end
+
+function __switch_nvm
   set splitted_path (string split / $PWD)
   set nvm_present 0
 
-  # echo (string join / $splitted_path[1..3])
   for i in (seq (count $splitted_path))
-    if test -e (string join / $splitted_path[1..$i+1]).nvmrc
-      set nvm_present 1
+    set to_test (string join / $splitted_path[1..$i+1]).nvmrc
+    if test -e $to_test
+      set nvm_present $to_test
     end
   end
 
-  if test "$nvm_present" -eq 1
-    set NVM_USED 1
-    nvm use
-  else if test "$NVM_USED" -eq 1
-    set NVM_USED 0
-    nvm use default
+  if test "$nvm_present" != 0 && test "$nvm_present" != "$NVMRC_USED"
+    set NVMRC_USED "$nvm_present"
+    nvm use > /dev/null
+  else if test "$NVMRC_USED" != 0 && test "$nvm_present" = 0
+    set NVMRC_USED 0
+    nvm use default > /dev/null
   end
 end
