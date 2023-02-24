e168bcab72449923306ce007afc91825f2852aff
Thibault Lacroux <thibault.lacroux@gmail.com>
2023-02-24 11:06:52 +0100

    fix install

--- install.fish
+++ install.fish
@@ -1,17 +1,24 @@
 #!/usr/bin/env fish
 
-if not test -L ~/.config/fish/conf.d
-    ln -s (pwd)/fish/conf.d ~/.config/fish/conf.d
+if test -d ~/.config/fish/conf.d/
+    cp -R ~/.config/fish/conf.d/* (pwd)/fish/conf.d/
+    rm -rf ~/.config/fish/conf.d
 end
+ln -s (pwd)/fish/conf.d ~/.config/fish/
 
 if not test -L ~/.config/fish/fishfile
     ln -s (pwd)/fish/fishfile ~/.config/fish/fishfile
 end
 
+set plugins (cat (pwd)/fish/fishfile | string split split0)
+set -l plugins_to_install
+for i in $plugins
+    set -a plugins_to_install (string replace ' ' '' $i)
+end
+
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    fisher install $to_install
 end
 
 set required docker exa asdf
